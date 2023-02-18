7bed52544888fa2088b3eef9810f147e616c7201
Can Surer <cansurer@outlook.com>
2023-02-18 23:06:18 +0100

    employee revise

--- server.js
+++ server.js
@@ -5,22 +5,13 @@ const cors = require('cors');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
+const corsOptions = require("./config/corseOptions");
 
 // custom middleware logger
 app.use(logger);
 
 // Cross Origin Resource Sharing
-const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200
-}
+
 app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded data
@@ -34,11 +25,9 @@ app.use(express.json());
 //serve static files
 app.use("/", express.static(path.join(__dirname, "/public")));
 
-app.use("/subdir", express.static(path.join(__dirname, "/public")));
 
 //routes
 app.use("/", require("./routes/root"));
-app.use("/subdir", require("./routes/subdir"));
 app.use("/employees", require("./routes/api/employees"));
 
 app.all('*', (req, res) => {
