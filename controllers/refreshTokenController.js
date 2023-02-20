b8a5c965a5a19488484a10dad3257a1b2d4f0017
Can Surer <cansurer@outlook.com>
2023-02-20 22:55:55 +0100

    user roles add

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -13,19 +13,18 @@ const handleRefreshToken = (req, res) => {
   const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
   if (!foundUser) return res.sendStatus(403); //Forbidden 
   // evaluate jwt 
-  jwt.verify(
-      refreshToken,
-      process.env.REFRESH_TOKEN_SECRET,
-      (err, decoded) => {
-          if (err || foundUser.username !== decoded.username) return res.sendStatus(403);
-          const accessToken = jwt.sign(
-              { "username": decoded.username },
-              process.env.ACCESS_TOKEN_SECRET,
-              { expiresIn: '30s' }
-          );
-          res.json({ accessToken })
-      }
-  );
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+    if (err || foundUser.username !== decoded.username)
+      return res.sendStatus(403);
+    const roles = Object.values(foundUser.roles);
+
+    const accessToken = jwt.sign(
+      { UserInfo: { username: decoded.username, roles: roles } },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "30s" }
+    );
+    res.json({ accessToken });
+  });
 }
 
 module.exports = { handleRefreshToken };
--- controllers/registerController.js
+++ controllers/registerController.js
@@ -13,19 +13,23 @@ const handleNewUser = async (req, res) => {
   const duplicate = usersDB.users.find(person => person.username === user);
   if (duplicate) return res.sendStatus(409); //Conflict 
   try {
-      //encrypt the password
-      const hashedPwd = await bcrypt.hash(pwd, 10);
-      //store the new user
-      const newUser = { "username": user, "password": hashedPwd };
-      usersDB.setUsers([...usersDB.users, newUser]);
-      await fsPromises.writeFile(
-          path.join(__dirname, '..', 'model', 'users.json'),
-          JSON.stringify(usersDB.users)
-      );
-      console.log(usersDB.users);
-      res.status(201).json({ 'success': `New user ${user} created!` });
+    //encrypt the password
+    const hashedPwd = await bcrypt.hash(pwd, 10);
+    //store the new user
+    const newUser = {
+      username: user,
+      roles: { User: 2001 },
+      password: hashedPwd,
+    };
+    usersDB.setUsers([...usersDB.users, newUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersDB.users)
+    );
+    console.log(usersDB.users);
+    res.status(201).json({ success: `New user ${user} created!` });
   } catch (err) {
-      res.status(500).json({ 'message': err.message });
+    res.status(500).json({ message: err.message });
   }
 }
 
