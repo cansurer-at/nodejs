55d3276e9f0981a340f6e27e3bbf99dcb48f8c7a
Can Surer <cansurer@outlook.com>
2023-02-21 16:45:53 +0100

    basic fix

--- config/corseOptions.js
+++ config/corseOptions.js
@@ -1,4 +1,10 @@
-const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const whitelist = [
+  "https://www.yoursite.com",
+  "http://localhost:3000",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+  "http://localhost:3500/register",
+];
 const corsOptions = {
   origin: (origin, callback) => {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
@@ -8,6 +14,7 @@ const corsOptions = {
     }
   },
   optionsSuccessStatus: 200,
+  credentials: true, // Set this to true to allow cookies to be sent with the request
 };
 
-module.exports = corsOptions; 
+module.exports = corsOptions;
